ee7f16d824970cc70e3c43b483645a797d3ef765
Ngo Tu <hoaitu1611@gmail.com>
2021-07-07 22:26:59 +0700

    mediaplayer backup 070721

---
+++ MediaPlayer/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace MediaPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.loadBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(721, 356);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            this.axWindowsMediaPlayer1.TabStop = false;
+            // 
+            // loadBtn
+            // 
+            this.loadBtn.Location = new System.Drawing.Point(13, 375);
+            this.loadBtn.Name = "loadBtn";
+            this.loadBtn.Size = new System.Drawing.Size(75, 23);
+            this.loadBtn.TabIndex = 1;
+            this.loadBtn.Text = "&Load";
+            this.loadBtn.UseVisualStyleBackColor = true;
+            this.loadBtn.Click += new System.EventHandler(this.loadBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 408);
+            this.Controls.Add(this.loadBtn);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Media Player App";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button loadBtn;
+    }
+}
+
---
+++ MediaPlayer/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediaPlayer
+{
+    public partial class Form1 : Form, IMessageFilter
+    {
+        private bool enableShortcut = false;
+        public Form1()
+        {
+            InitializeComponent();
+            //axWindowsMediaPlayer1.uiMode = "invisible";
+        }
+
+        private void loadBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                axWindowsMediaPlayer1.URL = ofd.FileName;
+                enableShortcut = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Application.AddMessageFilter(this);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.RemoveMessageFilter(this);
+        }
+
+        #region IMessageFilter
+
+        private const UInt32 WM_KEYDOWN = 0x0100;
+
+        public bool PreFilterMessage(ref Message m)
+
+        {
+            if (enableShortcut)
+            {
+                if (m.Msg == WM_KEYDOWN)
+                {
+
+                    Keys keyCode = (Keys)(int)m.WParam & Keys.KeyCode;
+
+                    /* Exit Fullscreen */
+                    if (keyCode == Keys.Escape)
+                    {
+
+                        this.axWindowsMediaPlayer1.fullScreen = false;
+
+                    }
+                    /* Fullscreen */
+                    else if (keyCode == Keys.F)
+                    {
+                        this.axWindowsMediaPlayer1.fullScreen = true;
+
+                    }
+                    /* Pause and Play */
+                    else if (keyCode == Keys.Space)
+                    {
+                        if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPlaying)
+                        {
+                            axWindowsMediaPlayer1.Ctlcontrols.pause();
+                        }
+                        else if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPaused)
+                        {
+                            axWindowsMediaPlayer1.Ctlcontrols.play();
+                        }
+                    }
+                    /*  */
+                    return true;
+
+                }
+            }
+
+            return false;
+
+        }
+
+        #endregion
+    }
+}
